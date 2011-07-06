a6a0cf085e7524f88e6f8631c55b0895f0f4e2b4
jhonny romero <jromeroarenales@gmail.com>
2011-07-06 19:24:25 -0430

    modificacione en la clase figura

--- Tombstone/src/Modelo/Modelo.java
+++ Tombstone/src/Modelo/Modelo.java
@@ -7,22 +7,30 @@ import java.util.List;
 public class Modelo {
 	
 	private List<Figura> listaFiguras;
+        private boolean esta;
 	public Modelo(){
 		listaFiguras = new ArrayList<Figura>();
+                esta=false;
 	}
-        
 	public List<Figura> getListado(){
 		return listaFiguras;
 	}
-        
 	public void anyadirFigura(Figura f){
 		listaFiguras.add(f);
 	}
-        
 	public void ElimminarFigura(Figura f){
             listaFiguras.remove(f);
         }
-        
+        public void EliminarLista(){
+            listaFiguras.clear();
+        }
+        public boolean EstaEnLista(Figura f){
+            
+            if(listaFiguras.contains(f)){
+                esta=true;
+            }else esta=false;
+            return esta;
+        }
 	public Figura getFiguraEn(Point p){
 		for (Figura elemento : getListado()) {
 			if(elemento.dentroFigura(p)){
