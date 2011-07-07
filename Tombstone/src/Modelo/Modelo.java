f42818facc15e75f64faa56865951e691b334f22
jhonny romero <jromeroarenales@gmail.com>
2011-07-06 20:59:33 -0430

    creacion de la lista auxiliar metiendo las figuras armadas

--- Tombstone/src/Modelo/Modelo.java
+++ Tombstone/src/Modelo/Modelo.java
@@ -7,17 +7,28 @@ import java.util.List;
 public class Modelo {
 	
 	private List<Figura> listaFiguras;
-        private boolean esta;
+        private List<Figura> listaAux;
+        private boolean esta,estaAux;
 	public Modelo(){
 		listaFiguras = new ArrayList<Figura>();
+                listaAux = new ArrayList<Figura>();
                 esta=false;
 	}
 	public List<Figura> getListado(){
 		return listaFiguras;
 	}
+        public List<Figura> getListadoAux(){
+		return listaAux;
+	}
+        public void anyadirFiguraAux(Figura f){
+		listaAux.add(f);
+	}
 	public void anyadirFigura(Figura f){
 		listaFiguras.add(f);
 	}
+        public void ElimminarFiguraAux(Figura f){
+            listaAux.remove(f);
+        }
 	public void ElimminarFigura(Figura f){
             listaFiguras.remove(f);
         }
@@ -30,6 +41,12 @@ public class Modelo {
                 esta=true;
             }else esta=false;
             return esta;
+        }public boolean EstaEnListaAux(Figura f){
+            
+            if(listaAux.contains(f)){
+                estaAux=true;
+            }else estaAux=false;
+            return estaAux;
         }
 	public Figura getFiguraEn(Point p){
 		for (Figura elemento : getListado()) {
